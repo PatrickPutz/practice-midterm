947cc589cf646abcd76654fd17beabfb49aa4a51
PatrickPutz <Putz.Patrick@outlook.com>
2021-11-23 19:45:38 +0100

    prepared methods for class 'Person'

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -35,4 +35,34 @@ public class Person {
     public void setAktivitaetslevel(byte aktivitaetslevel) {
         this.aktivitaetslevel = aktivitaetslevel;
     }
+
+    public String rechneBMI(int gewicht){
+        double bmi = (double) gewicht / (((double) this.koerpergroesse / 100) * ((double) this.koerpergroesse / 100));
+
+        if(bmi < 18.5)
+            return "Untergewicht";
+        else if(bmi <= 25)
+            return "Normalgewicht";
+        else if(bmi <= 30)
+            return "Übergewicht";
+        else
+            return "Adipositas";
+    }
+
+    public void printPerson(int gewicht){
+        String activitylevel;
+        if(this.aktivitaetslevel == 1)
+            activitylevel = "Couch Potato";
+        else if(this.aktivitaetslevel == 2)
+            activitylevel = "Gelegenheitssportler";
+        else if(this.aktivitaetslevel == 3)
+            activitylevel = "Sportler";
+        else if(this.aktivitaetslevel == 4)
+            activitylevel = "Profisportler";
+        else
+            activitylevel = "";
+
+        System.out.println(this.vorname + " " + this.nachname + ", " + activitylevel + ", " + this.rechneBMI(gewicht));
+    }
+
 }
